8f64957c5f84b02ee80c4705ab396a58a1384143
Hugo Burbelo <hugoburbelo@gmail.com>
2020-04-26 15:09:45 +0000

    Add Ebike complication classes

--- main.php
+++ main.php
@@ -1,7 +1,9 @@
 <?php
 const TR_METHOD_BIKE = "Bike";
+const TR_METHOD_EBIKE = "EBike";
 const TR_METHOD_CAR = "Car";
 
+// All valid methods of transportation extend this class
 class TransportationMethod {
   public $speed = 10;
 }
@@ -11,12 +13,17 @@ class Bike extends TransportationMethod {
   public $name = TR_METHOD_BIKE;
 }
 
+class EBike extends Bike {
+  public $speed = 30;
+  public $name = TR_METHOD_EBIKE;
+}
+
 class Car extends TransportationMethod {
   public $speed = 30;
   public $name = TR_METHOD_CAR;
 }
 
-
+// All edge types extend this class, and override the valid transportation methods
 class Edge {
   public $supportedTransportMethods = [];
   public $distance;
@@ -28,9 +35,9 @@ class Edge {
   }
 
   public function __construct($dist, $vert1, $vert2) {
-      $this->distance = $dist;
-      $this->vertice1 = $vert1;
-      $this->vertice2 = $vert2;
+    $this->distance = $dist;
+    $this->vertice1 = $vert1;
+    $this->vertice2 = $vert2;
   }
 }
 
@@ -42,11 +49,19 @@ class PavedRoad extends Edge {
   public $supportedTransportMethods = [TR_METHOD_BIKE, TR_METHOD_CAR];
 }
 
+class BikeTrail extends Edge {
+  public $supportedTransportMethods = [TR_METHOD_BIKE, TR_METHOD_EBIKE];
+}
+
+class BikeTrailNoEbike extends BikeTrail {
+  public $supportedTransportMethods = [TR_METHOD_BIKE];
+}
+
 class Vertice {
   public $name;
 
   function __construct($name) {
-        $this->name = $name;
+    $this->name = $name;
   }
 }
 
@@ -64,9 +79,10 @@ class Map {
     }
 }
 
+
+
+
 // example implementation
-
-
-$edges = [new Sidewalk(10, "A", "B"), new PavedRoad(20, "B", "C"), new PavedRoad(100, "A", "C")];
+$edges = [new Sidewalk(10, "A", "B"), new PavedRoad(20, "B", "C"), new PavedRoad(100, "A", "C"), new BikeTrailNoEbike(40, "C", "A")];
 $vertices = ["A", "B", "C"];
 $map = new Map($vertices, $edges);
