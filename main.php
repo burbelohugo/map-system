cfac3bc868a9f83306d9c2652b59dbeed4ce9fc9
Hugo Burbelo <hugoburbelo@gmail.com>
2020-04-26 15:25:13 +0000

    Add getMap method to map

--- main.php
+++ main.php
@@ -2,6 +2,10 @@
 const TR_METHOD_BIKE = "Bike";
 const TR_METHOD_EBIKE = "EBike";
 const TR_METHOD_CAR = "Car";
+const EDGE_SIDEWALK = "Sidewalk";
+const EDGE_PAVEDROAD = "PavedRoad";
+const EDGE_BIKE_TRAIL = "BikeTrail";
+const EDGE_BIKE_TRAIL_NO_EBIKE = "BikeTrailNoEbike";
 
 // All valid methods of transportation extend this class
 class TransportationMethod {
@@ -29,6 +33,7 @@ class Edge {
   public $distance;
   public $vertice1;
   public $vertice2;
+  public $name;
 
   function getSupportedTransportMethods(){
     return $this->supportedTransportMethods;
@@ -38,23 +43,44 @@ class Edge {
     $this->distance = $dist;
     $this->vertice1 = $vert1;
     $this->vertice2 = $vert2;
+    $this->name = "Edge";
+  }
+
+  public function __toString() {
+    return sprintf("%s with distance %s, from %s to %s\n", $this->name, $this->distance, $this->vertice1, $this->vertice2);
   }
 }
 
 class Sidewalk extends Edge {
-  public $supportedTransportMethods = [TR_METHOD_BIKE];
+  public function __construct($dist, $vert1, $vert2) {
+    parent::__construct($dist, $vert1, $vert2);
+    $this->supportedTransportMethods = [TR_METHOD_BIKE];
+    $this->name = EDGE_SIDEWALK;
+  }
 }
 
 class PavedRoad extends Edge {
-  public $supportedTransportMethods = [TR_METHOD_BIKE, TR_METHOD_CAR];
+  public function __construct($dist, $vert1, $vert2) {
+    parent::__construct($dist, $vert1, $vert2);
+    $this->supportedTransportMethods = [TR_METHOD_BIKE, TR_METHOD_CAR];
+    $this->name = EDGE_PAVEDROAD;
+  }
 }
 
 class BikeTrail extends Edge {
-  public $supportedTransportMethods = [TR_METHOD_BIKE, TR_METHOD_EBIKE];
+  public function __construct($dist, $vert1, $vert2) {
+    parent::__construct($dist, $vert1, $vert2);
+    $this->supportedTransportMethods = [TR_METHOD_BIKE, TR_METHOD_EBIKE];
+    $this->name = EDGE_BIKE_TRAIL;
+  }
 }
 
 class BikeTrailNoEbike extends BikeTrail {
-  public $supportedTransportMethods = [TR_METHOD_BIKE];
+  public function __construct($dist, $vert1, $vert2) {
+    parent::__construct($dist, $vert1, $vert2);
+    $this->supportedTransportMethods = [TR_METHOD_BIKE];
+    $this->name = EDGE_BIKE_TRAIL_NO_EBIKE;
+  }
 }
 
 class Vertice {
@@ -77,12 +103,27 @@ class Map {
     function getShortestDistance($vertA, $vertB, $transportMethod) {
       // path finding algorithm
     }
+
+    function getMap() {
+      print(implode($this->edges));
+    }
 }
 
 
 
 
-// example implementation
-$edges = [new Sidewalk(10, "A", "B"), new PavedRoad(20, "B", "C"), new PavedRoad(100, "A", "C"), new BikeTrailNoEbike(40, "C", "A")];
+// Example implementation
+
+// Each edge contains a distance, plus a starting and ending vertice
+$edges = [
+  new Sidewalk(10, "A", "B"), 
+  new PavedRoad(20, "B", "C"), 
+  new PavedRoad(100, "A", "C"), 
+  new BikeTrail(120, "A", "C"), 
+  new BikeTrailNoEbike(40, "C", "A")
+];
+// list of vertices
 $vertices = ["A", "B", "C"];
-$map = new Map($vertices, $edges);
+$exampleMap = new Map($vertices, $edges);
+
+$exampleMap->getMap();
